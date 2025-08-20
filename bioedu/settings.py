0caf9407f1ce938b5ab4e8dc29748b07bf6435be
dias-home-fedora <shaymerdenov.dias@bk.ru>
2025-08-20 20:07:13 +0500

    Editing project

--- bioedu/settings.py
+++ bioedu/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from django.utils.translation import gettext_lazy as _
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -43,6 +44,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
             ],
         },
     },
@@ -61,12 +63,16 @@ AUTH_PASSWORD_VALIDATORS = []
 
 AUTH_USER_MODEL = 'users.User'
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = "ru"  # язык по умолчанию
 LANGUAGES = [
-    ('ru', 'Русский'),
-    ('en', 'English'),
-    ('kk', 'Қазақша'),
+    ('ru', _('Russian')),
+    ('en', _('English')),
+    ('kk', _('Kazakh')),
 ]
+LOCALE_PATHS = [
+    BASE_DIR / "locale",  # сюда будут сохраняться переводы
+]
+
 TIME_ZONE = 'Asia/Almaty'
 USE_I18N = True
 USE_TZ = True
--- bioedu/urls.py
+++ bioedu/urls.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+    path("i18n/", include("django.conf.urls.i18n")),  # Переключатель языка
+]
+
+
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('main.urls')), # Главная страница и прочее
     path('users/', include('users.urls')), # Пользователи
     path('tests/', include('tests.urls')), # Тесты
-]
+)
 
 # Для статики
 if settings.DEBUG:
